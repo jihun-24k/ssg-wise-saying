2aa2ee1947a49f696a269d197d114dd0bb10134b
jihun-24k <gi020477@gmail.com>
2022-07-31 14:30:10 +0900

    Test: 모니터 출력을 문자열로 설정

--- src/main/java/com/exam/TestUtil.java
+++ src/main/java/com/exam/TestUtil.java
@@ -1,7 +1,6 @@
 package com.exam;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Scanner;
 
 public class TestUtil {
@@ -11,5 +10,22 @@ public class TestUtil {
         InputStream in = new ByteArrayInputStream(input.getBytes());
 
         return new Scanner(in);
+    }
+
+    public static ByteArrayOutputStream setOutToByteArray() {
+        // System.out: 원래 default값이 console로 반환하는 PrintStream
+        // System.setOut: PrintStream의 값을 바꿈
+        final ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+        return output;
+    }
+
+    public static void clearSetOutToByteArray(ByteArrayOutputStream output) {
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+        try {
+            output.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/test/java/com/exam/AppTest.java
+++ src/test/java/com/exam/AppTest.java
@@ -2,6 +2,7 @@ package com.exam;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -17,5 +18,15 @@ public class AppTest {
 
         String cmd = sc.nextLine().trim();
         assertEquals("안녕", cmd);
+    }
+    @Test
+    public void Print_NotMonitor_YesString() {
+        // ByteArrayOutputStream이 있는 상황엔 모니터의 문자열이 안에 output에 저장된다.
+        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+        System.out.print("안녕");
+        String rs = output.toString();
+        TestUtil.clearSetOutToByteArray(output);
+
+        assertEquals("안녕", rs);
     }
 }
